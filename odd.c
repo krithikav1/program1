319caad67dc301887db24aa5ae111f4ec623fc8b
krithikav1 <krithivaradharaj@gmail.com>
2017-06-05 16:03:53 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+# include<conio.h>
+void main()
+{
+int n;
+printf("Enter the number");
+scanf("%d",&n);
+if(n>0)
+printf("%d is positive number",n);
+else if(n<0)
+printf("%d is negative number",n);
+else
+printf("%d is zero",n);
+
+getch();
+}
